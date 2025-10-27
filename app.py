0dcbf609f8f78017736f190e3d9fc97eb3b0b9f8
Irfan786Shaik <116110674+Irfan786Shaik@users.noreply.github.com>
2025-10-27 18:27:18 +0530

    Updated templates, added static/css folder, and cleaned up old files

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,url_for,request,redirect,make_response
+from datetime import datetime
 app=Flask(__name__)
 users={}
+statements={}
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,9 +13,10 @@ def Create_Account():
         usermail=request.form['usermail']
         userpassword=request.form['userpassword']
         pinnumber=request.form['pinNumber']
-        if username not in users:
+        if username not in users and username not in statements:
             users[username]={'emailid':usermail,'userpassword':userpassword,'pin':pinnumber,'amount':0 }
-            return users
+            statements[username]={'deposit':[],'withdraw':[]}
+            return redirect(url_for('Login'))
         else:
             return 'username already exists '
     return render_template('Register.html')
@@ -42,7 +45,7 @@ def home():
         username=request.cookies.get('username')
         return render_template('home.html',username=username) 
     else:
-        return redirect('login.html')
+        return redirect(url_for('Login'))
 @app.route('/deposit',methods=['GET','POST'])
 def deposit():
      if request.cookies.get('username'):
@@ -53,7 +56,9 @@ def deposit():
                 if amount<50000:
                     if amount%100==0:
                         users[username]['amount']+=amount
-                        return users[username]
+                        deposittime=datetime.now()
+                        statements[username]['deposit'].extend((amount,deposittime))
+                        return redirect(url_for('balance'))
                     else:
                         return f'{amount} the amount should be multiply of 100'
                 else:
@@ -62,7 +67,7 @@ def deposit():
                 return f'{amount} the amount should be greater than 0'
         return render_template('deposit.html')
      else:
-         return redirect('login.html')
+         return redirect(url_for('Login'))
 @app.route('/withdraw',methods=['GET','POST'])
 def withdraw():
     if request.cookies.get('username'):
@@ -74,7 +79,9 @@ def withdraw():
                     if amount<useramount:
                         if amount%100==0:
                             users[username]['amount']-=amount
-                            return users[username]
+                            withdrawtime=datetime.now()
+                            statements[username]['withdraw'].extend((amount,withdrawtime))
+                            return redirect(url_for('balance'))
                         else:
                             return "amount not multiple of 100"
                     else:
@@ -83,11 +90,24 @@ def withdraw():
                 return "amount entered is negative"
         return render_template('withdrawal.html')
     else:
-        return redirect('02_login.html')
+        return redirect(url_for('Login'))
 @app.route('/balance',methods=['GET'])
 def balance():
     if request.cookies.get('username'):
         username=request.cookies.get('username')
         balanceAmount=users[username]['amount']
         return render_template('balance.html',Balance=balanceAmount)
+    else:
+        return redirect(url_for('Login'))
+@app.route('/mini_statements',methods=['GET'])
+def mini_statements():
+    if request.cookies.get('username'):
+        username=request.cookies.get('username')
+        deposit_statements=statements[username]['deposit']
+        print(deposit_statements)
+        withdraw_statements=statements[username]['withdraw']
+        print(withdraw_statements)
+        return render_template('statement.html')
+    else:
+        return redirect(url_for('Login'))
 app.run(use_reloader=True,debug=True)
